d31de1cbfab3d7272c85d61c83281f8d33ccb017
kachraz <kachraz@tuta.io>
2024-08-27 15:26:43 +0530

    .

--- g.sh
+++ g.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
-git add .
-git commit -m "♓2nd"
-git push
+echo "==================="
+echo ""
+echo "git add -all && git commit -m <addurshit>&& git push"
+git add --all 
+git commit -m "."
+git push --progress
+echo ""
+echo "=========PUSH DONE========="
+echo ""
+echo "==================="
+echo "View Git log last 10"
+echo ""
+git log --oneline --graph --decorate -10 
+echo ""
+echo "=========LOGDONE========="
+echo ""
+echo " View all branches "
+echo ""
+git branch -a 
+echo ""
+echo "=========BranchesDone========="
+echo ""
+echo "View Status "
+git status 
+echo ""
+echo "=========StatusDone========="
