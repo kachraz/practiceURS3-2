0f74551c86db3b731bae016550738e65368a901c
kachraz <kachraz@tuta.io>
2024-08-28 23:18:53 +0530

    ♓2ndPusy

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { CORE_CONCEPTS } from "./data";
 import Header from "./comp/Header/Header";
 import CoreConcept from "./comp/CoreCo/CoreCo";
 import TabButton from "./comp/TabComponents/TabButton.";
+import { EXAMPLES } from "./comp/TabComponents/data-with-example.js";
 
 // -------------------
 
@@ -45,7 +46,13 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {slectedTopic}
+          <div id="tab-component">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
